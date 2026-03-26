64f07a9a5aa788852adfc120f9d82ab4cf8ca6c2
Ajith Mogaveera <72547043+AjithMogaveeraSooral@users.noreply.github.com>
2026-03-26 20:58:54 +0530

    Script url

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const registrationForm = document.getElementById('registrationForm');
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbyU9hO4UNwuVZd2VPkDFPsVg8F24OSS-cxAwDxk_Pl_YO4pLJf3dwxpsRsG2gcA_dohuA/exec'; // <--- Paste your App Script URL here
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwn2mWbAeELEhHT6QI2MW5Ppt1hJHwLYcMdjY5CcLT6SN8D2SwKUgP_Ap4WLBomQrLdzQ/exec'; // <--- Paste your App Script URL here
 
     registrationForm.addEventListener('submit', function(e) {
         e.preventDefault();
